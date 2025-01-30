7de8b354b9b3a10cfc14f2e729994ae20e1f9e2b
BeLikeYEJOR <yejor599@gmail.com>
2025-01-29 22:13:17 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import NewBtn from "./components/setButton.vue";
+import CloseSVG from "./components/icons/closeSVG.vue";
+import { ref, watch } from "vue";
+const name = "World";
+let txt = ref("");
+function clearText() {
+  txt.value = "";
+}
+watch(txt, (newValue) => {
+  otherBtn.onclick = () => {
+    window.open(`https://${newValue}.com`, "_black").focus();
+  };
+});
+</script>
+
+<template>
+  <h1>Type In Whatever You Want!</h1>
+  <input
+    type="text"
+    id="buttonText"
+    v-model="txt"
+    placeholder="Type In This Box!"
+  />
+  <a id="closeSvg" @click="clearText"><CloseSVG></CloseSVG></a>
+  <NewBtn id="otherBtn" class="green">{{ txt }}</NewBtn>
+</template>
+
+<style scoped>
+#otherBtn {
+  position: relative;
+  bottom: 70px;
+  color: #00bd7e;
+  transition: 0.4s;
+  padding: 3px;
+}
+input[type="text"] {
+  margin: 20px;
+  margin-right: 0;
+  margin-left: 0;
+  padding: 10px;
+  border-radius: 20px;
+  outline: none;
+  border: 3px solid #313131;
+  background-color: #4e4e4e;
+  color: white;
+}
+#closeSvg {
+  position: relative;
+  bottom: 63px;
+  left: 75px;
+  border-radius: 200px;
+}
+#closeSvg:hover {
+  background-color: red;
+  filter: blur(20px);
+  box-shadow: 0 0 3px 3px red;
+}
+</style>
---
+++ src/components/setButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="toDo">
+    <button class="green"><slot></slot></button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  background-color: #2b2b2b;
+  border: 1px solid #313131;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
+
+<script setup></script>
